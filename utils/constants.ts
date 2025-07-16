ef8e3937732ca3e2d7fa6f1ad895c61b62d3bd1e
Omar Kohl <omarkohl@posteo.net>
2025-07-16 22:43:01 +0200

    lwcw: set relevant constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -7,15 +7,12 @@ export type NavItem = {
 };
 
 export const CONSTS = {
-  TITLE: "Example Conference Weekend",
+  TITLE: "LWCW 2025",
   DESCRIPTION:
-    "An Example Conference is happening from August 23 - 25, 2024 at Lighthaven. Check out the schedules for the main event and afterparty below!",
-  MULTIPLE_EVENTS: true,
+    "Berlin",
+  MULTIPLE_EVENTS: false,
   // If you have multiple events, add your events to the nav bar below
   // If you only have one event, you can leave the array empty
   // Find available icons at https://heroicons.com/
-  NAV_ITEMS: [
-    { name: "Conference", href: "/Conference", icon: UserGroupIcon },
-    { name: "After Party", href: "/After-Party", icon: CakeIcon },
-  ] as NavItem[],
+  NAV_ITEMS: [] as NavItem[],
 };
