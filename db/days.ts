c394c3bc09fe6ded5ac45a9b246759c9f1e629e9
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2025-07-25 15:07:12 +0200

    Deduplicate code

--- db/days.ts
+++ db/days.ts
@@ -23,37 +23,13 @@ type DayRecord = {
 };
 
 export async function getDays() {
-  const days: Day[] = [];
-  const fieldsToFetch: (keyof DayRecord)[] = [
-    "Start",
-    "End",
-    "Start bookings",
-    "End bookings",
-  ];
-  if (CONSTS.MULTIPLE_EVENTS) {
-    fieldsToFetch.push("Event name", "Event");
-  }
-  await base<DayRecord>("Days")
-    .select({
-      fields: fieldsToFetch,
-    })
-    .eachPage(function page(records, fetchNextPage) {
-      records.forEach(function (record) {
-        days.push({ ...record.fields, Sessions: [], ID: record.id });
-      });
-      fetchNextPage();
-    });
-  const sortedDays = days.sort((a, b) => {
-    return new Date(a.Start).getTime() - new Date(b.Start).getTime();
-  });
-  return sortedDays;
+  return await getDaysByEvent(null);
 }
 
-export async function getDaysByEvent(eventName: string) {
+export async function getDaysByEvent(eventName: string | null) {
   const days: Day[] = [];
-  const filterFormula = CONSTS.MULTIPLE_EVENTS
-    ? `{Event name} = "${eventName}"`
-    : "1";
+  const filterFormula =
+    CONSTS.MULTIPLE_EVENTS && eventName ? `{Event name} = "${eventName}"` : "1";
   const fieldsToFetch: (keyof DayRecord)[] = [
     "Start",
     "End",
